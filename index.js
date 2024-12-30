5e369f5d0c640e79731ada2010cd9f7feeb70210
swaggyhustler <gedamsubhash086@gmail.com>
2024-12-30 16:03:01 +0530

    Adding temp origin 1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 app.use(cors({
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173/"]
 }));
 
 app.use(express.urlencoded({ extended: true }));
